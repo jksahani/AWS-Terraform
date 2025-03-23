76fcbcb7128fd1cbfcb26d612d562259ab537c79
jksahani <46551637+jksahani@users.noreply.github.com>
2025-03-23 10:59:08 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.92.0"
+    }
+  }
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+provider "aws" {
+  # Configuration options
 }
+
+/*
+resource "aws_s3_bucket" "storage" {
+  bucket = "terraforms3321313"
+}
+*/
+
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.1.1.0/24"
+}
+
+
